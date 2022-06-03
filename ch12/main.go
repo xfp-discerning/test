dd2ed78c4936bb7ebee052c0c33653a03c512c06
xfp-discerning <xfp.discerning@gmail.com>
2022-06-03 23:13:03 +0800

    interface

---
+++ ch12/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+type mysql struct{}
+
+type redis struct{}
+
+func (r *redis) Get(name string) (*User, error) {
+	return &User{Name: "Bob"}, nil
+}
+
+type User struct {
+	Name string
+}
+
+type Store interface {
+	Get(name string) (*User, error)
+}
+
+type Server struct {
+	store Store
+}
+
+func (s *Server) User(name string) {
+	user, err := s.store.Get(name)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	fmt.Println(user)
+}
+
+func (s *Server) Run() error {
+	return nil
+}
+
+func main() {
+	store := redis{}
+	s := Server{
+		store: &store,
+	}
+	s.User("Bob")
+}
